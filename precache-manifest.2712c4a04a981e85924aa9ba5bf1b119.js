fe10007a6c9d44d65987d629d0528db6ac8021e2
Ali Sheikh <ali.sheikh@flexport.com>
2020-08-23 15:06:12 -0700

    Updates

---
+++ precache-manifest.2712c4a04a981e85924aa9ba5bf1b119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7548b114b1374846777ef9fb8e2842d3",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "b3d6a91ccfb11554aa80",
+    "url": "/deploy-react/static/css/main.c0559acf.chunk.css"
+  },
+  {
+    "revision": "90f98c0522496a556b1e",
+    "url": "/deploy-react/static/js/2.70fa21a2.chunk.js"
+  },
+  {
+    "revision": "b3d6a91ccfb11554aa80",
+    "url": "/deploy-react/static/js/main.fc772a37.chunk.js"
+  },
+  {
+    "revision": "a70ab81c67defc9fa7dc",
+    "url": "/deploy-react/static/js/runtime~main.5de6419e.js"
+  }
+]);
